313b4ac480b19725cb290cc2df7566b34e1561fb
Joshua Retallick <josh@old-empire.co.uk>
2021-03-15 11:11:58 +0000

    Returns a total when more than one item is scanned

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -7,4 +7,9 @@ describe Checkout do
   it "gives a total when an item is scanned" do
     expect(shop.checkout('A')).to eq 50
   end
+
+  it "gives a total when two items are scanned" do
+    expect(shop.checkout('AB')).to eq 80
+  end
+  
 end
