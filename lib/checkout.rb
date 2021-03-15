547620bc8750e973a86e013bcc4fb1af1ef1e18c
Joshua Retallick <josh@old-empire.co.uk>
2021-03-15 13:52:52 +0000

    Complete solution

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,20 +1,34 @@
 class Checkout
 
-  def checkout(item)
-    items = {
-      'A' => 50,
-      'B' => 30,
-      'C' => 20,
-      'D' => 15
-    }
-    receipt = 0
+  def checkout(items)
+    return -1 unless items.kind_of? String
+    return -1 if items != items.upcase || items =~ /\d/
+    @cost = 0
+    calc(items.chars)
+    special_offer(items.chars)
+    @cost
+  end
 
-    order= item.upcase.split('')
+  private
 
-    order.each do |i|
-      receipt += items[i]
+  def special_offer(items)
+    @cost -= 20 * (items.count('A') / 3)
+    @cost -= 15 * (items.count('B') / 2)
+  end
+
+  def calc(items)
+    items.each do |item|
+      @cost += case item
+      when 'A'
+        50
+      when 'B'
+        30
+      when 'C'
+        20
+      when'D'
+        15
+      end
     end
-    return receipt
   end
 
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,18 +2,58 @@ require 'checkout'
 
 describe Checkout do
 
-  let(:shop) { described_class.new }
+  describe '#checkout' do
+    it 'returns -1 when receives lower case letters' do
+      expect(subject.checkout('aBc')).to eq -1
+    end
 
-  it "gives a total when an item is scanned" do
-    expect(shop.checkout('A')).to eq 50
-  end
+    it 'returns -1 when receives numbers in string' do
+      expect(subject.checkout('AB8C')).to eq -1
+    end
 
-  it "gives a total when two items are scanned" do
-    expect(shop.checkout('AB')).to eq 80
-  end
+    it 'returns -1 when receives integer' do
+      expect(subject.checkout(3)).to eq -1
+    end
 
-  it "Considers lowercase inouts when processing an order" do
-    expect(shop.checkout('abcd')).to eq 115
+    context 'add to cost' do
+      it '50 for every "A"' do
+        expect(subject.checkout('A')).to eq 50
+        expect(subject.checkout('AA')).to eq 100
+      end
+
+      it '30 for every "B"' do
+        expect(subject.checkout('B')).to eq 30
+        expect(subject.checkout('BB')).to eq 45
+      end
+
+      it '20 for every "C"' do
+        expect(subject.checkout('C')).to eq 20
+        expect(subject.checkout('CC')).to eq 40
+      end
+
+      it '15 for every "D"' do
+        expect(subject.checkout('D')).to eq 15
+        expect(subject.checkout('DD')).to eq 30
+      end
+    end
+
+    context 'special offer' do
+      it '3A for 130' do
+        expect(subject.checkout('AAA')).to eq 130
+        expect(subject.checkout('AAAAAA')).to eq 260
+      end
+
+      it '2B for 45' do
+        expect(subject.checkout('BB')).to eq 45
+        expect(subject.checkout('BBBB')).to eq 90
+      end
+
+      it 'totals for various combinations' do
+        expect(subject.checkout('BBAAA')).to eq 175
+        expect(subject.checkout('BBBBCDCDDAAAAAAA')).to eq 485
+      end
+    end
+
   end
 
 end
